82559cf0ab7086ae44e74d1ba992ba37208c77e0
Joanna Zhou <joanna.zyz@outlook.com>
2019-01-22 20:49:37 -0500

    Tested the data import

--- kNN/kNN-Algorithm.py
+++ kNN/kNN-Algorithm.py
@@ -1,7 +1,9 @@
-'''
-Implement the k-NN algorithm for regression with three different distance metrics (l2, l1, and l∞). Use 5-fold cross-validation1 to estimate k, and the preferred distance metric using a root-mean-square error (RMSE) loss. Compute nearest neighbours using a brute-force approach.
-Apply your algorithm to all regression datasets (use n train=1000, d=2 for rosenbrock). For each dataset, report the estimated value of k and the preferred distance metric, and report the cross-validation RMSE and test RMSE with these settings. Format these results in a table.
-Plot the cross-validation prediction curves (merging the predictions from all splits) for the one-dimensional regression dataset mauna loa at several values of k for the l2 distance metric. In separate figures, plot the prediction on the test set, as well as the cross-validation loss across k for this model. Discuss your results.
-'''
 # import pandas
 from data_utils import load_dataset
+
+
+if __name__ == '__main__':
+    x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset('mauna_loa')
+    # print(str(x_train[2][0])+', '+str(y_train[2][0]))
+    xy_train_list = ['x: ' + str(x_train[i][0]) + ', y: ' + str(y_train[i][0]) for i in range(0, len(x_train))]
+    print("\n".join(xy_train_list))
